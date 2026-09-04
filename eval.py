f9790536b82b347b93eb60d5bf59ef25cbafa091
Dave Barragan <itsdaveba@gmail.com>
2026-09-04 11:57:12 +0100

    evaluate over a range of masses

--- eval.py
+++ eval.py
@@ -32,10 +32,16 @@ if __name__ == "__main__":
 
     model = PPO.load(os.path.join(filename, "best_model"), device="cpu")
 
-    n_eval_episodes = 30
-    target_pos = np.array([0.0, 0.0, 0.5])
-    env_kwargs = dict(gui=False, num_drones=1, initial_spawn=0.5, target_pos=target_pos, act=ActionType.RPM)
-    env = make_vec_env(HoverAviary, n_envs=n_eval_episodes, env_kwargs=env_kwargs)
+    n_eval_episodes = 100
+    env_kwargs = dict(initial_spawn=0.5, target_pos=np.array([0.0, 0.0, 0.5]), act=ActionType.RPM)
 
-    episode_rewards, episode_lengths = evaluate_policy(model, env, n_eval_episodes, True, return_episode_rewards=True)
-    print(episode_rewards, episode_lengths)
+    for mass in np.arange(0.017, 0.038, 0.001):
+        env_kwargs["mass"] = mass
+        env = make_vec_env(HoverAviary, n_envs=5, env_kwargs=env_kwargs)
+        episode_rewards, _ = evaluate_policy(model, env, n_eval_episodes, True, return_episode_rewards=True)
+        df = pd.DataFrame(dict(episode_rewards=episode_rewards, mass=env.envs[0].env.M))
+        df.to_csv(os.path.join(filename, "evaluations.csv"), header=not os.path.exists(os.path.join(filename, "evaluations.csv")), index=False, mode="a")
+
+    df = pd.read_csv(os.path.join(filename, "evaluations.csv"))
+    sns.lineplot(df, x="mass", y="episode_rewards")
+    plt.show()
