2e47d7b9435d279b332a5777f14c8f0a232701b0
Dave Barragan <itsdaveba@gmail.com>
2026-09-18 09:18:37 +0100

    context visible

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for kf in $(seq 1.66e-10 3.25e-11 8.16e-10); do
-    python eval.py --n-eval-episodes 10 --start 0.007 --stop 0.047 --step 0.002 --kwarg mass --context-kwargs kf --context-low-gen $kf --context-high-gen $kf --final-model
+    python eval.py --n-eval-episodes 10 --start 0.007 --stop 0.047 --step 0.002 --kwarg mass --context-kwargs kf --context-low-gen $kf --context-high-gen $kf --context-visible 1 1 --context-low 1.66e-10 0.007 --context-high 8.16e-10 0.047
 done
--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python train.py --total-timesteps 106496 --context-kwargs mass kf --context-low-gen 0.027 4.91e-10 --context-high-gen 0.027 4.91e-10
+python train.py --total-timesteps 106496 --context-kwargs mass kf --context-low-gen 0.027 4.91e-10 --context-high-gen 0.027 4.91e-10 --context-visible 1 1 --context-low 0.007 1.66e-10 --context-high 0.047 8.16e-10
 
 experiment_id=$(tail -n 1 results/experiments.txt)
 for i in {1..9}; do
@@ -8,5 +8,5 @@ for i in {1..9}; do
     mass_high=$(printf "%.4f\n" $(echo "0.027 + $i / 450" | bc -l))
     kf_low=$(printf "%.4e\n" $(echo "0.000000000491 - $i / 27692000000" | bc -l))
     kf_high=$(printf "%.4e\n" $(echo "0.000000000491 + $i / 27692000000" | bc -l))
-    python train.py --total-timesteps 106496 --experiment-id $experiment_id --context-kwargs mass kf --context-low-gen $mass_low $kf_low --context-high-gen $mass_high $kf_high
+    python train.py --total-timesteps 106496 --experiment-id $experiment_id --context-kwargs mass kf --context-low-gen $mass_low $kf_low --context-high-gen $mass_high $kf_high --context-visible 1 1 --context-low 0.007 1.66e-10 --context-high 0.047 8.16e-10
 done
